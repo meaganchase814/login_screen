8d8bf6829ee3fd336db846250e7cc52d8aac1056
Meagan Chase <meaganchase814@yahoo.com>
2019-02-25 20:04:51 -0500

    styled personal info page

--- reactnative/githubapp/src/components/SignUp/PersonalInfo.js
+++ reactnative/githubapp/src/components/SignUp/PersonalInfo.js
@@ -7,6 +7,7 @@ export default class PersonalInfo extends Component {
       
         this.state = {
             language: " ",
+            text: ''
           };
       
           // this.drawerToggleClickHandler = this.drawerToggleClickHandler.bind(this);
@@ -14,57 +15,106 @@ export default class PersonalInfo extends Component {
     }
     render() {
         return(
-            <KeyboardAvoidingView behavior="padding" style={styles.container}>
-            <View style={styles.container}>
-                <View style={styles.inputWrapper} >
-                    <Text>Add a short bio</Text>
-                    <TextInput 
-                            placeholder="bio"
-                            placeholderTextColor='rgba(255, 255, 255, 0.2)'
-                            returnKeyType='next'
-                            onSubmitEditing={() => this.passwordInput.focus()}
-                            keyboardType="email-address"
-                            autoCapitalize="none"
-                            autoCorrect={false}
-                            style={styles.input}
-                    />
-                    <View>
-                        <Text>Gender</Text>
-                        <TouchableHighlight
-                            title="male"
-                        >
-                            <Text>M</Text>
-                        </TouchableHighlight>
-                    </View>
-                    <Text>Birthday</Text>
-                    <Picker
-                        selectedValue={this.state.language}
-                        style={{height: 50, width: 100}}
-                        onValueChange={(itemValue, itemIndex) =>
-                            this.setState({language: itemValue})
-                        }>
-                        <Picker.Item label="Jan" value="Jan" />
-                        <Picker.Item label="Feb" value="Feb" />
-                        <Picker.Item label="March" value="March" />
-                        <Picker.Item label="April" value="April" />
-                        <Picker.Item label="May" value="May" />
-                    </Picker>
+          
+                <KeyboardAvoidingView behavior="padding" style={styles.container}>
+                    <View style={styles.container}>
+                        <View style={styles.inputWrapper} >
+                            <View>
+                                <Text>Add a short bio</Text>
+                                <TextInput 
+                                    placeholder="bio"
+                                    placeholderTextColor='rgba(255, 255, 255, 0.2)'
+                                    returnKeyType='next'
+                                    keyboardType="email-address"
+                                    autoCapitalize="none"
+                                    autoCorrect={false}
+                                    multiline = {true}
+                                    numberOfLines = {4}
+                                    onChangeText={(text) => this.setState({text})}
+                                    value={this.state.text}
+                                    style={styles.input}    
+                                />
+                            </View>
+                            <View>
+                                <Text>Gender</Text>
+                                <View style={{flexDirection: 'row'}}>
+                                <TouchableHighlight
+                                    title="male"
+                                    style={{borderWidth: 1, padding: 2, margin: 2}}
+                                >
+                                    <Text>M</Text>
+                                </TouchableHighlight>    
+                                <TouchableHighlight
+                                    title="female"
+                                    style={{borderWidth: 1, padding: 3, margin: 2}}
+                                >
+                                    <Text>F</Text>
+                                </TouchableHighlight>  
+                                <TouchableHighlight
+                                    title="other"
+                                    style={{borderWidth: 1, padding: 2, margin: 2}}
+                                >
+                                    <Text>O</Text>
+                                </TouchableHighlight>  
+                                </View>
+                            </View>
+                        <View>
+                            <Text>Birthday</Text>
+                            <View style={{flexDirection: 'row'}}>
+                                <Picker
+                                    selectedValue={this.state.language}
+                                    style={styles.picker}
+                                    onValueChange={(itemValue, itemIndex) =>
+                                        this.setState({language: itemValue})
+                                    }>
+                                    <Picker.Item label="Jan" value="Jan" />
+                                    <Picker.Item label="Feb" value="Feb" />
+                                    <Picker.Item label="March" value="March" />
+                                    <Picker.Item label="April" value="April" />
+                                    <Picker.Item label="May" value="May" />
+                                </Picker>
+                                <Picker
+                                    selectedValue={this.state.language}
+                                    style={styles.picker}
+                                    onValueChange={(itemValue, itemIndex) =>
+                                        this.setState({language: itemValue})
+                                    }>
+                                    <Picker.Item label="1" value="1" />
+                                    <Picker.Item label="2" value="2" />
+                                    <Picker.Item label="3" value="3" />
+                                    <Picker.Item label="4" value="4" />
+                                    <Picker.Item label="5" value="5" />
+                                </Picker>
+                                <Picker
+                                    selectedValue={this.state.language}
+                                    style={styles.picker}
+                                    onValueChange={(itemValue, itemIndex) =>
+                                        this.setState({language: itemValue})
+                                    }>
+                                    <Picker.Item label="1980" value="1980" />
+                                    <Picker.Item label="1990" value="1990" />
+                                    <Picker.Item label="2000" value="2000" />
+                                </Picker>
+                            </View>
+                            </View>
+                            
 
-                </View>
-                <View style={styles.buttonWrapper} >
-                    <View>
-                        <TouchableOpacity 
-                        title='Next'
-                        onPress={() => this.props.navigation.navigate('Home')}
-                        >
-                        <Text style={styles.buttonContainer}>Next</Text>
-                        </TouchableOpacity>
+                    
+                            <View style={styles.buttonWrapper} >
+                                <View>
+                                    <TouchableOpacity 
+                                    title='Next'
+                                    onPress={() => this.props.navigation.navigate('Home')}
+                                    >
+                                    <Text style={styles.buttonContainer}>Next</Text>
+                                    </TouchableOpacity>
+                                </View>
+                            </View>                    
+                        </View>
                     </View>
-                                           
-                </View>
-            </View>
-        
-        </KeyboardAvoidingView> 
+            
+                </KeyboardAvoidingView> 
+           
         )
     };
 
@@ -76,7 +126,7 @@ const styles = StyleSheet.create({
         width: Dimensions.get('window').width,
         flex: 1,
         backgroundColor: '#3498db',
-        
+        flexWrap: 'wrap',
     },
     logoContainer: {
         alignItems: 'center',
@@ -111,28 +161,28 @@ const styles = StyleSheet.create({
         padding: 0,
     },
     titleWrapper: {
-        flex: 1, 
-        
+        flex: 1,
         flexDirection:'row',
        justifyContent: 'center'
     },
     input: {
-        height: 40,
+        height: 100,
         backgroundColor: 'rgba(255, 255, 255, 0.2)',
         marginBottom: 10,
         color: '#FFF',
         paddingHorizontal: 10,
         marginLeft: 10,
         marginRight: 10,
-        borderRadius: 50,
-        width: 100,
+        width: 300,
+        
+        flexWrap: 'wrap'
         
     },
     inputWrapper: {
         flexGrow: 1,
-        flexDirection: 'row',
+        flexDirection: 'column',
         alignItems: 'center',
-        justifyContent: 'center',
+        marginTop: 20,
         flexWrap: 'wrap'
     },
     buttonContainer: {
@@ -145,9 +195,9 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         textAlign: 'center',
         margin: 1,
-        borderRadius: 50,
         height: 20,
         paddingTop: 2,
+        width: 50,
       },
       buttonWraper: {
         flexGrow: 1, 
@@ -156,6 +206,11 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
        
       },
+    picker: {
+        height: 50, 
+        width: 120,
+        alignItems: 'center'
+    },
     
 })
     
